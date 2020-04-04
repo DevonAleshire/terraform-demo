4b8be8f175851948174a5544e469433601e44015
Devon Aleshire <devon.aleshire@gmail.com>
2020-04-04 10:06:36 -0700

    Add tfvars example file and update main tf

--- azurerm/d1/main.tf
+++ azurerm/d1/main.tf
@@ -1,3 +1,31 @@
+##################
+# Variables
+##################
+variable azure_subscriptionId {}
+variable azure_appId {}
+variable azure_password  {}
+variable azure_tenant  {}
+
+variable "region" {
+  type    = string
+  default = "West US"
+}
+
+##################
+# Provider
+##################
 provider "azurerm"{
     features {}
+    subscription_id = var.azure_subscriptionId
+    client_id      = var.azure_appId  
+    client_secret  = var.azure_password
+    tenant_id      = var.azure_tenant
+}
+
+##################
+# Resources
+##################
+resource "azurerm_resource_group" "tf-demo"{
+    name     = "tf-demo-rg"
+    location = var.region
 }
